baa57fba91df86177c2667a30015aeffeab46877
Karim Badwork <karim@badwork.fr>
2025-09-29 02:30:16 +0100

    Refactor code structure for improved readability and maintainability

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+	"users-permissions": {
+		config: {
+			jwtSecret:
+				"8qKjF9hB7nS4mR3lW2jP1oN0iYxZcVaT7uE6dC5bA4zY3xV8rQW0eM9pL7kI6jH5gY4fE3dC2bA1zY0w=",
+		},
+	},
+});
--- config/server.js
+++ config/server.js
@@ -1,10 +1,16 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+	host: env("HOST", "0.0.0.0"),
+	port: env.int("PORT", 1337),
+	app: {
+		keys: ["jdiVRmuuxOpvSvr/cAbJyg==", "cfT/u+SsPjau/z2vPK0+bQ=="],
+	},
+	apiToken: {
+		salt: "AFlAihDgHLkOsJeXKogKbA==",
+	},
+	jwt: {
+		secret: "8qKjF9hB7nS4mR3lW2jP1oN0iYxZcVaT7uE6dC5bA4zY3xV8rQW0eM9pL7kI6jH5gY4fE3dC2bA1zY0w=",
+	},
+	webhooks: {
+		populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+	},
 });
